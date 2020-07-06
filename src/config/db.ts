00c79737c542e216236debe385bccca3edcf77df
tajud99n <giwa208@gmail.com>
2020-07-06 18:15:57 +0100

    set up database

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+import { logger } from "./logger";
+
+
+const URI = config.environment === "test" ? config.mongodb.test_uri : config.mongodb.uri;
+
+export const connectDB = async () => {
+	try {
+		await mongoose.connect(URI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+
+		logger.info("MongoDB Connected...");
+	} catch (err) {
+		logger.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
